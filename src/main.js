acade429c435bcf69b4fb79f6e22a379c4c89a30
陈紫柠 <czn_xmut@163.com>
2021-07-19 19:48:13 +0800

    项目初始化-vant引入

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './stylee/index.less'
+import './stylee/icon.less'
+// 方式3 全部加入加入核心组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 注册使用vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
